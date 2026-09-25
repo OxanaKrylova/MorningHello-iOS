1d83adea59847ca221ab652645ec065f2f56ce77
OxanaKrylova <krylov.oxana@gmail.com>
2026-09-26 00:06:11 +0300

    создан API по профилю с питомцем

---
+++ Backend/ProfileDataAPIClient.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileDataAPIClient.swift
+//  MorningHello
+//
+//  Created by Oxana Krylova on 25/09/2026.
+//
+
+import Foundation
---
+++ Backend/ProfileDataModels.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileDataModels.swift
+//  MorningHello
+//
+//  Created by Oxana Krylova on 25/09/2026.
+//
+
+import Foundation
